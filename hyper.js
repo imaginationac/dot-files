a1c33669287c545511ccd5235bf54095611af513
Dorian Patterson <imaginationac@gmail.com>
2017-04-15 21:46:00 +0900

    hyper.js: Change font family to Cousine for Powerline.

--- hyper.js
+++ hyper.js
@@ -8,7 +8,7 @@ module.exports = {
 		fontSize: 13,
 
 		// font family with optional fallbacks
-		fontFamily: 'Source Code Pro for Powerline, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+		fontFamily: 'Cousine for Powerline, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
 		// terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
 		cursorColor: 'rgba(248,28,229,0.8)',
