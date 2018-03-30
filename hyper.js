cd2eb719843b02fd9a9bc0cbbd0365d8344ed142
Dorian Patterson <imaginationac@gmail.com>
2018-03-30 23:03:00 +0900

    hyper.js: Reformat plugins listing.

--- hyper.js
+++ hyper.js
@@ -105,7 +105,12 @@ module.exports = {
 	//   `hyperpower`
 	//   `@company/project`
 	//   `project#1.0.1`
-	plugins: ["hyperterm-summon", "hypercwd", "hyperlinks-iterm", "hyperterm-clicky"],
+	plugins: [
+        "hyperterm-summon",
+        "hypercwd",
+        "hyperlinks-iterm",
+        "hyperterm-clicky",
+    ],
 
 	// in development, you can create a directory under
 	// `~/.hyper_plugins/local/` and include it here
